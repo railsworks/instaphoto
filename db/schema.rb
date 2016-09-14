5f2d98879abb28f708c8e726d7c83d695834c36d
Konstantin <railsworks@yahoo.com>
2016-09-14 16:23:55 +0300

    Добавлен атрибут name для User, добавлены валидации для User и Photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908124157) do
+ActiveRecord::Schema.define(version: 20160914111720) do
 
   create_table "photos", force: :cascade do |t|
     t.string   "description"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160908124157) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
