6dd77a320cd835e3cc26ccfc1703f3ef1148e59c
Konstantin <railsworks@yahoo.com>
2016-10-03 13:02:30 +0300

    Добавлены Devise Confirmable и почтовые настройки 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true  }
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'instaphoto-rails.herokuapp.com' }
 
 
   config.paperclip_defaults = {
